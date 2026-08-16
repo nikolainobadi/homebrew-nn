8c42bc9aa6cf1e1b8fb2e335f96109bbc0c4eb98
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2026-08-15 18:26:22 -0700

    Update shellboss to 0.9.0

--- Casks/shellboss.rb
+++ Casks/shellboss.rb
@@ -1,8 +1,8 @@
 cask "shellboss" do
-    version "0.8.0"
-    sha256 "4ef250e893ddaf46fc8ae4ace8aae9cb34d6c84fae475c24526574987539d949"
+    version "0.9.0"
+    sha256 "88f57c0e878ef14c78cb6605d91ca773d6f5c0affca1edf88bfcf735800cf225"
 
-    url "https://github.com/nikolainobadi/homebrew-nn/releases/download/shellboss-0.8.0/ShellBoss.dmg"
+    url "https://github.com/nikolainobadi/homebrew-nn/releases/download/shellboss-0.9.0/ShellBoss.dmg"
     name "ShellBoss"
     desc "A macOS terminal app with tabs, todos, and MCP integration"
     homepage "https://github.com/nikolainobadi/ShellBoss"
